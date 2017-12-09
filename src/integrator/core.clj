58203bf8eb61cd9f97ea1bffcc26f659f6f6edbc
Julius Eerola <6095076+tahkojuusto@users.noreply.github.com>
2017-12-09 23:24:25 +0200

    Refactored integration calculation

--- src/integrator/core.clj
+++ src/integrator/core.clj
@@ -3,6 +3,8 @@
 
 (defn -run
     [f x0 x1 N]
+
+    ; Run separately using different methods.
     (let [trapezoid-report (proc/integrate f x0 x1 "trapezoid" N)
           midpoint-report (proc/integrate f x0 x1 "midpoint" N)
           simpson-report (proc/integrate f x0 x1 "simpson" N)]
@@ -11,7 +13,9 @@
         (println simpson-report)))
 
 (defn -main
-    "I don't do a whole lot ... yet."
+    "Integrate f(x) from x0 to x1 with N steps. Use multiple numerical methods."
     [& args]
-    (let [[f x0 x1 N] args]
-        (-run f x0 x1 N)))
+    (if (not (= (count args) 4))
+        (println "Usage: (-main (fn [x] ( ... )) x0 x1 N)")
+        (let [[f x0 x1 N] args]
+            (-run f x0 x1 N))))
--- src/integrator/process.clj
+++ src/integrator/process.clj
@@ -1,62 +1,83 @@
 (ns integrator.process)
 
-(defn -integrate-trapezoid
-    "Calculate integrate of f(x) (i.e. area A) from x0 to x1 using interval A.
-    Use the trapezoid rule."
-    [f x0 x1 x dx A]
-
-    ; Iterate until x reaches the upper limit x1.
-    (if (>= x x1)
-        A
-        (let [f0 (f x)
-              f1 (f (+ x dx))
-              dA (* (/ (+ f0 f1) 2) dx)]
-            (recur f x0 x1 (+ x dx) dx (+ A dA)))))
-
-(defn -integrate-midpoint
-    "Calculate integrate of f(x) (i.e. area A) from x0 to x1 using interval A.
-    Use the midpoint rule."
-    [f x0 x1 x dx A]
-
-    ; Iterate until x reaches the upper limit x1.    
-    (if (>= x x1)
-        A
-        (let [fm (f (+ x (/ dx 2)))
-              dA (* fm dx)]
-            (recur f x0 x1 (+ x dx) dx (+ A dA)))))
-
+;; Methods for calculating integration steps
+;; #########################################
+(defn -step-midpoint
+    "Midpoint rule (0th order polynomial)."
+    [f state]
+    (let [{:keys [x0 x1 x dx sum i]} state
+          fm (f (+ x (/ dx 2)))]
+        
+        ; Form a horizontal line at the midpoint.
+        (* fm dx)))
+        
+(defn -step-trapezoid
+    "Trapezoid rule (1th order polynomial)."
+    [f state]
+    (let [{:keys [x0 x1 x dx sum i]} state
+          f0 (f x)
+          f1 (f (+ x dx))]
+        
+        ; Form a line between (x0, f0) - (x1, f1) and take its
+        ; value at the midpoint.
+        (* (/ (+ f0 f1) 2) dx)))
 
 (defn -integrate-simpson-coefficient
-    "Determine coefficient (2 or 4) in the middle terms of Simpson rule."
+    "Determine coefficient of a middle term in Simpson rule (2 or 4)."
     [i]
     (let [even? (= (mod i 2) 0)]
         (if even? 2 4)))
 
-(defn -integrate-simpson
-    "Calculate integrate of f(x) (i.e. area A) from x0 to x1 using interval A.
-    Use the Simpson rule."
-    [f x0 x1 x dx A i]
+(defn -step-simpson
+    "Simpson rule (2nd order polynomial)."
+    [f state]
+    (let [{:keys [x0 x1 x dx sum i]} state
+          coef  (-integrate-simpson-coefficient i)
+          f0    (f x0)
+          f1    (f x1)
+          fm    (* coef (f x))]
+        
+        ; Form 2nd order polynomial between (x0, f0) - (x1, f1),
+        ; and take its value at the midpoint.
+        (cond (= x x0) (/ (* f0 dx) 3)
+              (= x x1) (/ (* f1 dx) 3)
+              :else (/ (* fm dx) 3))))
 
-    ; Iterate until x reaches the upper limit x1.
-    (if (>= x x1)
-        A
-        (let [coef (-integrate-simpson-coefficient i)
-              f0 (f x0)
-              f1 (f x1)
-              fm (* coef (f x))
-              dA (cond (= x x0) (/ (* f0 dx) 3)
-                       (= x x1) (/ (* f1 dx) 3)
-                       :else (/ (* fm dx) 3))]
-            (recur f x0 x1 (+ x dx) dx (+ A dA) (+ i 1)))))
+;; ###########################################
+
+(defn -integrate-generic
+    "Sum steps calculated using specific method until x >= x1."
+    [f step state]
+    (let [{:keys [x0 x1 x dx sum i]} state]
+        (if (>= x x1)
+            sum
+
+            ; Recursion with an updated state.
+            ; The updated state has accumulated the new step value.
+            (recur f step (-> (assoc state :sum (+ sum (step f state)))
+                              (assoc :x (+ x dx))
+                              (assoc :i (+ i 1)))))))
+
+(defn -init-state
+    "Create state map, which has start point x0, end point x1, step size dx, current position x,
+     current area sum, and current index i."
+    [x0 x1 dx]
+    {:x0 x0, :x1 x1, :dx dx, :x x0, :sum 0, :i 0})
+
+(def -method-conversion
+    {"trapezoid" -step-trapezoid,
+     "midpoint" -step-midpoint,
+     "simpson" -step-simpson})
 
 (defn integrate
     "Integrate f(x) from x0 to x1. Use the given method with N samples."
-    [f x0 x1 method N]
+    [f x0 x1 method-str N]
     (let [dx (/ (- x1 x0) N)
-          A0 0
-          i0 0
-          result (cond (= method "trapezoid") (-integrate-trapezoid f x0 x1 x0 dx A0)
-                       (= method "midpoint") (-integrate-midpoint f x0 x1 x0 dx A0)
-                       (= method "simpson") (-integrate-simpson f x0 x1 x0 dx A0 i0)
-                       :else "ERR: Specific method not found!")]
-        (str method ":\t" (double result))))
+          ; Get the initial state map.
+          state (-init-state x0 x1 dx)
+          ; Get the function pointer based on the function name.
+          step-method (get -method-conversion method-str)
+          ; Calculate the integration.
+          result (-integrate-generic f step-method state)]
+        ; Print the result.
+        (str method-str ":\t" (double result))))
