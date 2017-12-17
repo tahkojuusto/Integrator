4c2628aec61c9a0cd70629a26f1db79433813fb2
Julius Eerola <6095076+tahkojuusto@users.noreply.github.com>
2017-12-17 11:30:52 +0200

    Removed non-needed files

--- doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to integrator
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
--- test/integrator/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns integrator.core-test
-  (:require [clojure.test :refer :all]
-            [integrator.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
